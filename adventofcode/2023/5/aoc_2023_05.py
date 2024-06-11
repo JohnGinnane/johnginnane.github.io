39d074819e9588548b04f74ca098aad247bdb9c5
John Ginnane <John.Ginnane1@Gmail.com>
2024-06-11 22:36:27 +0100

    Separated map range into its own class

--- adventofcode/2023/5/aoc_2023_05.py
+++ adventofcode/2023/5/aoc_2023_05.py
@@ -7,22 +7,60 @@ def right(s, l):
 def pad(s, l):
     return right(" " * l + str(s), l)
 
-class map:
-    def __init__(self, source_type, source_start, dest_type, dest_start, range):
-        self.source_type = source_type
+class map_range:
+    def __init__(self, source_start, dest_start, range):
         self.source_start = source_start
-        self.dest_type = dest_type
         self.dest_start = dest_start
         self.range = range
+
+    def __str__(self):
+        output  = "\n" + pad(self.source_start, 5) + " -> " + pad(self.dest_start, 5) + " ... " + pad(self.source_start + self.range, 5) + " -> " + pad(self.dest_start + self.range, 5)
+        return output
+
+class map:
+    def __init__(self, source_type, dest_type):
+        self.source_type = source_type
+        self.destination_type = dest_type
+        self.ranges = []
+
+    def addMapRange(self, source_start, dest_start, new_range):
+        # Keep ranges in order pls
+        found = False
+        for r in self.ranges:
+            if source_start == r.source_start:
+                found = True
+                break
+
+        if not found:
+            new_map_range = map_range(source_start, dest_start, new_range)
+            self.ranges.append(new_map_range)
+            
+            # No recorded ranges, just slap it in
+            # if len(self.ranges) <= 0:
+            #     self.ranges.append(new_map_range)
+            # else:
+            #     # Try to place in order
+            #     found = False
+            #     for k in range(len(self.ranges)):
+            #         v = self.ranges[k]
+            #         if v.source_start > source_start:
+            #             found = True
+            #             self.ranges.insert(k, v)
+            #             break
+
+            #     # None found, just slap it in
+            #     if not found:
+            #         self.ranges.append(new_map_range)
+
 
     def __str__(self):
         output = ""
 
         output += "FROM: " + pad(self.source_type, 12)
-        output +=  " TO: " + pad(self.dest_type, 12)
-        output += " SRC: " + pad(self.source_start, 4)
-        output += " DST: " + pad(self.dest_start, 4)
-        output += " RNG: " + pad(self.range, 4)
+        output +=  " TO: " + pad(self.destination_type, 12)
+
+        for r in self.ranges:
+            output += "\t" + str(r)
 
         return output
 
@@ -30,12 +68,11 @@ lines = open("test_05.txt", "r").readlines()
 seeds = []
 maps = []
 
+for k in range(3):
+    print(k)
+
 mapping = False
-map_from = ""
-map_to = ""
-map_destination = 0
-map_source = 0
-map_range = 0
+last_map_index = -1
 
 for line in lines:
     if line.strip() == "":
@@ -54,17 +91,34 @@ for line in lines:
         map_match = re.match(r"([a-zA-Z]+)-to-([a-zA-Z]+) map:", line)
         
         if map_match:
-            map_from = map_match.group(1)
-            map_to = map_match.group(2)
+            last_map_index = -1
+            source = map_match.group(1)
+            destination = map_match.group(2)
+
+            # find the match
+            for k in range(len(maps)):
+                v = maps[k]
+                if v.source_type == source and v.dest_type == destination:
+                    last_map_index = k
+                    break
+
+            # Couldn't find mapping, create a new one
+            if last_map_index < 0:
+                newmap = map(source, destination)
+                maps.append(newmap)
+                last_map_index = len(maps) - 1
+                #print("Creating a new map for " + str(newmap) + " at index " + str(last_map_index))
         else:
             map_match = re.match(r"([0-9]+) ([0-9]+) ([0-9]+)", line)
+            
+            if last_map_index >= 0 and last_map_index < len(maps) and map_match:
+                last_map = maps[last_map_index]
 
-            if map_match:
-                map_destination = int(map_match.group(1))
-                map_source = int(map_match.group(2))
-                map_range = int(map_match.group(3))
+                new_destination_start = int(map_match.group(1))
+                new_source_start = int(map_match.group(2))
+                new_range = int(map_match.group(3))
 
-                maps.append(map(map_from, map_source, map_to, map_destination, map_range))
+                last_map.addMapRange(new_source_start, new_destination_start, new_range)
 
 print(seeds)
 
