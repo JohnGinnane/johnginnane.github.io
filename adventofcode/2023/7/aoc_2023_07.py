3311d492a720a1a45358234c9a642d1acaa56bbf
John Ginnane <John.Ginnane1@Gmail.com>
2024-06-15 23:05:50 +0100

    groundwork for day 7

---
+++ adventofcode/2023/7/aoc_2023_07.py
@@ -0,0 +1,30 @@
+import re
+import math
+
+def right(s, l):
+    return str(s)[-l:]
+
+def left(s, l):
+    return str(s)[:l]
+
+def pad(s, l):
+    return right(" " * l + str(s), l)
+
+def padr(s, l):
+    return left(str(s) + " " * l, l)
+
+class hand:
+    def __init__(self, original):
+        self.original_string = original
+
+    def __str__(self):
+        print(self.original_string)
+
+lines = open("test_07.txt", "r").readlines()
+hands = []
+
+for line in lines:
+    hands.append(hand(line))
+
+for h in hands:
+    print(h)
