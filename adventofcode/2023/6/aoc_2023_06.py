2c4d3949e00948285112652782d5ab99d6e62d98
John Ginnane <John.Ginnane1@Gmail.com>
2024-06-15 12:42:10 +0100

    Fixed truncation error

--- adventofcode/2023/6/aoc_2023_06.py
+++ adventofcode/2023/6/aoc_2023_06.py
@@ -8,37 +8,37 @@ def pad(s, l):
     return right(" " * l + str(s), l)
 
 class race:
-    def __init__(self, time:float, distance:float):
+    def __init__(self, time:int, distance:int):
         self.time = time
         self.distance = distance
-        self.min_charge = 0.0
-        self.max_charge = 0.0
+        self.min_charge = 0
+        self.max_charge = 0
 
     def __str__(self):
-        return str(self.distance) + "mm@" + str(self.time) + "s - (" + str(self.min_charge) + " <= c <= " + str(self.max_charge) + ")"
+        return str(self.distance) + "mm@" + str(self.time) + "s - (" + str(self.min_charge) + " <= c <= " + str(self.max_charge) + ") - Diff: " + str(self.max_charge - self.min_charge)
 
-lines = open("test_06.txt", "r").readlines()
+lines = open("input_06.txt", "r").readlines()
 races = []
 
 time_matches = re.findall(r"([0-9]+)+", lines[0])
 dist_matches = re.findall(r"([0-9]+)+", lines[1])
 
 for k in range(len(time_matches)):
-    t = float(time_matches[k])
-    d = float(dist_matches[k])
+    t = int(time_matches[k])
+    d = int(dist_matches[k])
     races.append(race(t, d))
 
-ways = 0.0
+ways = 0
 
 for r in races:
-    c = 0.0
+    c = 0
     while c < r.time:
         # Iterate over the race for range(time)
         # Try to find min and max time for charging to achieve dist
-        c += 1.0
+        c += 1
 
         if (r.time - c) * c > r.distance:
-            if r.min_charge == 0.0:
+            if r.min_charge == 0:
                 r.min_charge = c
             
             r.max_charge = c
@@ -47,11 +47,13 @@ for r in races:
                 break
     
     # Part 1
+    diff = (r.max_charge - r.min_charge + 1)
+
     if ways == 0:
-        ways = (r.max_charge - r.min_charge + 1.0)
+        ways = diff
     else:
-        ways *= (r.max_charge - r.min_charge + 1.0)
+        ways *= diff
     
     print(r)
 
-print("Part 1 Number of Ways: " + pad(ways, 5))
+print("Part 1 Number of Ways: " + str(ways))
