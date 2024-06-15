64501784590ad0191c933692b315ef9061832a9c
John Ginnane <John.Ginnane1@Gmail.com>
2024-06-15 12:38:42 +0100

    Implemented solution for day 6 part 1

--- adventofcode/2023/6/aoc_2023_06.py
+++ adventofcode/2023/6/aoc_2023_06.py
@@ -11,20 +11,47 @@ class race:
     def __init__(self, time:float, distance:float):
         self.time = time
         self.distance = distance
+        self.min_charge = 0.0
+        self.max_charge = 0.0
 
     def __str__(self):
-        return str(self.distance) + "mm@" + str(self.time) + "s" 
+        return str(self.distance) + "mm@" + str(self.time) + "s - (" + str(self.min_charge) + " <= c <= " + str(self.max_charge) + ")"
 
-lines = open("test_05.txt", "r").readlines()
+lines = open("test_06.txt", "r").readlines()
 races = []
 
 time_matches = re.findall(r"([0-9]+)+", lines[0])
 dist_matches = re.findall(r"([0-9]+)+", lines[1])
 
 for k in range(len(time_matches)):
-    t = time_matches[k]
-    d = dist_matches[k]
+    t = float(time_matches[k])
+    d = float(dist_matches[k])
     races.append(race(t, d))
 
+ways = 0.0
+
 for r in races:
+    c = 0.0
+    while c < r.time:
+        # Iterate over the race for range(time)
+        # Try to find min and max time for charging to achieve dist
+        c += 1.0
+
+        if (r.time - c) * c > r.distance:
+            if r.min_charge == 0.0:
+                r.min_charge = c
+            
+            r.max_charge = c
+        else:
+            if r.max_charge > 0:
+                break
+    
+    # Part 1
+    if ways == 0:
+        ways = (r.max_charge - r.min_charge + 1.0)
+    else:
+        ways *= (r.max_charge - r.min_charge + 1.0)
+    
     print(r)
+
+print("Part 1 Number of Ways: " + pad(ways, 5))
