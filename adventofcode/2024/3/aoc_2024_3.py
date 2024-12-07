5f1548a4e6055d4ce6b431fe0a9eac27efee6191
John Ginnane <John.Ginnane1@Gmail.com>
2024-12-07 22:29:18 +0000

    Solved it!

--- adventofcode/2024/3/aoc_2024_3.py
+++ adventofcode/2024/3/aoc_2024_3.py
@@ -21,16 +21,31 @@ param_mul = re.compile(r"([0-9]{1,3})")
 
 muls = re.findall(regex_mul, input)
 
+# Count number of parameters instances
+dict_param = {}
+
 grand_total = 0
 for m in muls:
     params = re.findall(param_mul, m[0])
+
+    if len(params) < 2:
+        continue
+
+    if not (len(params) in dict_param.keys()):
+        dict_param[len(params)] = 1
+    else:
+        dict_param[len(params)] += 1
+    
     #   print(str(m) + " -> " + str(params))
     total = 1
     for n in params:
         total *= int(n)
     grand_total += total
     
-    print(m[0] + " = " + str(total))
+    #print(m[0] + " = " + str(total))
+
+print(dict_param)
 
 # 171183259 -- too high
+# Part 1: 171183089
 print("Grand Total: " + str(grand_total))
