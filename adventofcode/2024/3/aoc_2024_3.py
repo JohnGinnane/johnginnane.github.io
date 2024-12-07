2886d458f29ebd9424bde0e1ebc54b86a25c9372
John Ginnane <John.Ginnane1@Gmail.com>
2024-12-07 22:39:28 +0000

    Day 3 Part 3 solved, first try!!

--- adventofcode/2024/3/aoc_2024_3.py
+++ adventofcode/2024/3/aoc_2024_3.py
@@ -16,17 +16,31 @@ def sign(n:int):
 input = open("input_03.txt", "r").read()
 
 # Count instances of mul(
+input_mul = re.compile(r"(do\(\)|don't\(\)|mul\(([0-9]{1,3}(,|))+\))")
 regex_mul = re.compile(r"(mul\(([0-9]{1,3}(,|))+\))")
 param_mul = re.compile(r"([0-9]{1,3})")
 
-muls = re.findall(regex_mul, input)
+instruct = re.findall(input_mul, input)
 
 # Count number of parameters instances
 dict_param = {}
+enabled = True
 
 grand_total = 0
-for m in muls:
-    params = re.findall(param_mul, m[0])
+for i in instruct:
+    params = re.findall(param_mul, i[0])
+
+    # Check if it's a do or don't
+    if i[0] == "do()":
+        enabled = True
+    elif i[0] == "don't()":
+        print(i[0])
+        enabled = False
+
+    if not enabled:
+        continue
+
+    print(i[0])    
 
     if len(params) < 2:
         continue
@@ -48,4 +62,5 @@ print(dict_param)
 
 # 171183259 -- too high
 # Part 1: 171183089
+# Part 2: 63866497 -- first try qB^]
 print("Grand Total: " + str(grand_total))
