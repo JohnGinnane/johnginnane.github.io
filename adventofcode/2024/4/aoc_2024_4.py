45e7ba97d35fa9a7f55e659b64b24b19502c9da4
John Ginnane <John.Ginnane1@Gmail.com>
2024-12-08 01:42:00 +0000

    Turned code into function
    Need to compress rotated array back down to smallest size without cropping

--- adventofcode/2024/4/aoc_2024_4.py
+++ adventofcode/2024/4/aoc_2024_4.py
@@ -12,7 +12,7 @@ def pad_with(vector, pad_width, iaxis, kwargs):
     vector[:pad_width[0]]  = ' '
     vector[-pad_width[1]:] = ' '
 
-with open("test_input_04.txt", "r") as f:
+with open("test_input_04b.txt", "r") as f:
     data_raw = [list(line.strip()) for line in f]
 
 data = np.array(data_raw)
@@ -62,55 +62,39 @@ data = np.array(data_raw)
 # for r in data:
 #     print(r)
 
+def rotate(arr, deg):
+    size = len(data)
+    arr_pad = np.pad(arr, ((0, size * 3), (0, size * 3)), mode="constant", constant_values=' ')
 
-#print(data)
-#data = np.pad(data, 6, pad_with)
-size = len(data)
-data = np.pad(data, ((0, size * 3), (0, size * 3)), mode="constant", constant_values=' ')
-#print(data)
+    # x' = cos(45°) * x - sin(45°) * y
+    # y' = sin(45°) * x + cos(45°) * y
 
-# x' = cos(45°) * x - sin(45°) * y
-# y' = sin(45°) * x + cos(45°) * y
+    centerX = len(arr_pad[0]) / 2
+    centerY = len(arr_pad) / 2
 
-centerX = len(data[0]) / 2
-centerY = len(data) / 2
+    arr_expl = np.full((size * 4, size * 4), ' ', dtype=str)
+    arr_rota = np.full((size * 4, size * 4), ' ', dtype=str)
 
-test = ""
-newTest = ""
-deg = math.pi / 4
-exploded = np.full((size * 4, size * 4), ' ', dtype=str)
-rotated = np.full((size * 4, size * 4), ' ', dtype=str)
-pad_amt = 5
+    for y, row in enumerate(arr_pad):
+        for x, char in enumerate(row):
+            if char == ' ':
+                continue
+            arr_expl[y*2+size][x*2+size] = char
 
-for y, row in enumerate(data):
-    for x, char in enumerate(row):
-        if char == ' ':
-            continue
-        exploded[y*2+size][x*2+size] = char
+    for y, row in enumerate(arr_expl):
+        for x, char in enumerate(row):
+            relX = x - centerX
+            relY = y - centerY
 
-#print(exploded)
+            newX = (math.cos(deg) * relX - math.sin(deg) * relY) + centerX
+            newY = (math.sin(deg) * relX + math.cos(deg) * relY) + centerY
 
-for y, row in enumerate(exploded):
-    for x, char in enumerate(row):
-        relX = x - centerX
-        relY = y - centerY
-
-        newX = (math.cos(deg) * relX - math.sin(deg) * relY) + centerX
-        newY = (math.sin(deg) * relX + math.cos(deg) * relY) + centerY
-        test += "[" + pad(str(x), pad_amt) + ", " + pad(str(y), pad_amt) + "](" + char + ") "
-        round_amt = 3
-        newTest += "[" + pad(str(round(newX, round_amt)), pad_amt) + ", " + pad(str(round(newY, round_amt)), pad_amt) + "](" + char + ") "
-
-        if char != ' ':
-            rotated[round(newY)][round(newX)] = char
+            if char != ' ':
+                arr_rota[round(newY)][round(newX)] = char
         
-    test += "\n"
-    newTest += "\n"
+    return arr_rota
 
-# print(test)
-# print(newTest)
-
-# print(rotated)
+rotated = rotate(data, math.pi / 2)
 
 result = ""
 for y, row in enumerate(rotated):
