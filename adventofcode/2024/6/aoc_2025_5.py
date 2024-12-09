8cf90d824f07d85b3af813c9a48304b90812428f
John Ginnane <John.Ginnane1@Gmail.com>
2024-12-09 23:23:02 +0000

    First draft of day 6 part 1

---
+++ adventofcode/2024/6/aoc_2025_5.py
@@ -0,0 +1,92 @@
+print("test")
+
+def GridToString(grid):
+    result = ""
+    for row in grid:
+        for col in row:
+            result += col
+        result += "\n"
+    return result.strip()
+
+grid = []
+width = 0
+height = 0
+y = 0
+x = 0
+
+with open("test_input_06.txt", "r") as f:
+    lines = f.readlines()
+
+    for line in lines:
+        if len(grid) <= y:
+            grid.append([])
+
+        for char in line:
+            grid[y].append(char)
+
+            if x > width:
+                width = x
+            x += 1
+
+        x = 0
+        y += 1
+        if y > height:
+            height = y
+
+guard_pos = dict(x=0, y=0)
+guard_vel = dict(x=0, y=0)
+
+print("Width:  " + str(width))
+print("Height: " + str(height))
+
+# find guard
+for y, row in enumerate(grid):
+    for x, char in enumerate(row):
+
+        if char == '^' or char.lower() == 'v' or char == '<' or char == '>':
+            guard_pos["x"] = x
+            guard_pos["y"] = y
+
+            guard_vel["x"] = 0
+            guard_vel["y"] = 0
+
+            if   char == '^':
+                guard_vel["y"] = -1
+            elif char == '>':
+                guard_vel["x"] =  1
+            elif char.lower() == 'v':
+                guard_vel["y"] =  1
+            elif char == '<':
+                guard_vel["x"] = -1
+            
+            break
+
+visited = []
+
+while (guard_pos["x"] >= 0 and guard_pos["x"] < width and
+       guard_pos["y"] >= 0 and guard_pos["y"] < height):
+    # More guard in the direction they are facing until they hit an object
+    if not (guard_pos["x"], guard_pos["y"]) in visited:
+        visited.append((guard_pos["x"], guard_pos["y"]))
+
+    # Check object in front is an obstacle
+    try:
+        next = grid[guard_pos["y"] + guard_vel["y"]][guard_pos["x"] + guard_vel["x"]]
+    except:
+        # Out of bounds means the guard will leave the area
+        print("Guard has left the area, last seen at:")
+        print("Pos: [" + str(guard_pos["x"]) + ", " + str(guard_pos["y"]) + "]")
+        print("Heading in direction:")
+        print("Vel: [" + str(guard_vel["x"]) + ", " + str(guard_vel["y"]) + "]")
+        break
+
+    if next == '#':
+        # Rotate vel 90 degrees to the right
+        temp = guard_vel["x"]
+        guard_vel["x"] = -guard_vel["y"]
+        guard_vel["y"] =  temp
+    else:
+        guard_pos["x"] += guard_vel["x"]
+        guard_pos["y"] += guard_vel["y"]
+
+print("Distinct locations: " + str(len(visited)))
