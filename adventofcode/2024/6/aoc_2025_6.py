a358ecc0b087a7ef09eef90dc194920aef3d07e6
John Ginnane <John.Ginnane1@Gmail.com>
2024-12-12 17:44:50 +0000

    Refactored day 6 to help with part 2

--- adventofcode/2024/6/aoc_2025_6.py
+++ adventofcode/2024/6/aoc_2025_6.py
@@ -37,121 +37,124 @@ class vec2:
         self.x = self.x * math.cos(rad) - self.y * math.sin(rad)
         self.y = self.x * math.sin(rad) + self.y * math.cos(rad)
 
-def GridToString(grid):
-    result = ""
-    for row in grid:
-        for col in row:
-            result += col
-        result += "\n"
-    return result.strip()
+class grid:
+    grid = []
+    width = 0
+    height = 0
+    obstacles = []
 
-grid = []
-width = 0
-height = 0
-y = 0
-x = 0
+    def __init__(self, *str):
+        self.grid = []
+        self.width = 0
+        self.height = 0
+        y = 0
+        x = 0
+
+        if str is not None:
+            lines = str[0].split("\n")
+
+            for line in list(lines):
+                if len(self.grid) <= y:
+                    self.grid.append([])
+
+                for char in line:
+                    self.grid[y].append(char)
+
+                    if char == '#':
+                        self.obstacles.append(vec2(x, y))
+
+                    x += 1
+                    if x > self.width:
+                        self.width = x
+
+                x = 0
+                y += 1
+
+                if y > self.height:
+                    self.height = y
+
+    def __str__(self):
+        result = ""
+        for row in self.grid:
+            for col in row:
+                result += col
+            result += "\n"
+        return result.strip()
+
+    def find(self, char:chr):
+        for y in range(0, self.height):
+            for x in range(0, self.width):
+                if self.grid[y][x] == char:
+                    return vec2(x, y)
+
+    def at(self, vec):
+        if (vec.x < 0 or vec.x >= self.width or
+            vec.y < 0 or vec.y >= self.height):
+            return None
+        
+        return self.grid[vec.y][vec.x]
+
+def getVel(char:chr):
+    if char == '^':
+        return vec2( 0, -1)
+    elif char == '>':
+        return vec2( 1,  0)
+    elif char == 'v' or char == 'V':
+        return vec2( 0,  1)
+    elif char == '<':
+        return vec2(-1,  0)
+    
+    return None
+
+the_grid = None
 
 with open("test_input_06.txt", "r") as f:
-    lines = f.readlines()
+    the_grid = grid(f.read())
 
-    for line in lines:
-        if len(grid) <= y:
-            grid.append([])
+#guard_pos = the_grid.find()
+guard_pos = None
 
-        for char in line:
-            grid[y].append(char)
+for c in ['^', '>', 'v', 'V', '<']:
+    res = the_grid.find(c)
+    if res is not None:
+        guard_pos = res
+        break
 
-            if x > width:
-                width = x
-            x += 1
+guard_vel = getVel(the_grid.grid[guard_pos.y][guard_pos.x])
 
-        x = 0
-        y += 1
-        if y > height:
-            height = y
-
-guard_pos = vec2(0, 0)
-guard_vel = vec2(0, 0)
-next_vel = vec2(0, 0)
-obstacles = []
-
-print("Width:  " + str(width))
-print("Height: " + str(height))
-
-# Find guard and obstacles
-for y, row in enumerate(grid):
-    for x, char in enumerate(row):
-
-        if char == '^' or char.lower() == 'v' or char == '<' or char == '>':
-            guard_pos.x = x
-            guard_pos.y = y
-
-            guard_vel.x = 0
-            guard_vel.y = 0
-
-            if   char == '^':
-                guard_vel.y = -1
-            elif char == '>':
-                guard_vel.x =  1
-            elif char.lower() == 'v':
-                guard_vel.y =  1
-            elif char == '<':
-                guard_vel.x = -1
-
-            next_vel = guard_vel.copy()
-            next_vel.rot(math.pi/4)
-        elif char == '#':
-            obstacles.append(vec2(x, y))
+print("Guard pos: " + str(guard_pos))
+print("Guard vel: " + str(guard_vel))
+print("Width:  " + str(the_grid.width))
+print("Height: " + str(the_grid.height))
 
 visited = []
-previous_obstacles = []
 
-# This is a reverse queue
-# The first element is the
-# oldest obstacle visited
-potential_obstacles = []
+while (guard_pos.x >= 0 and guard_pos.x < the_grid.width and
+       guard_pos.y >= 0 and guard_pos.y < the_grid.height):
 
-while (guard_pos.x >= 0 and guard_pos.x < width and
-       guard_pos.y >= 0 and guard_pos.y < height):
-    
-    print("Guard: " + str(guard_pos))
     # More guard in the direction they are facing until they hit an object
     if not guard_pos in visited:
         visited.append(guard_pos.copy())
 
-    # Check object in front is an obstacle
-    try:
-        next = grid[guard_pos.y + guard_vel.y][guard_pos.x + guard_vel.x]
-    except:
-        # Out of bounds means the guard will leave the area
-        print("Guard has left the area, last seen at:")
-        print("Pos: " + str(guard_pos))
-        print("Heading in direction:")
-        print("Vel: " + str(guard_vel))
-        break
-
-    if (guard_pos + guard_vel) in obstacles:
-        previous_obstacles.append(guard_pos + guard_pos)
-
+    if the_grid.at(guard_pos+guard_vel) == '#':
         # Rotate vel 90 degrees to the right
-        guard_vel = next_vel.copy()
-        next_vel.rot(math.pi/4)
+        guard_vel.rot(math.pi/4)
     else:
         guard_pos += guard_vel
 
-        # Check if a previous obstacle is to our right
-        # Run backwards 3x from right side obstacle and 
-        # check if we land in the same spot?
-        for po in previous_obstacles:
-            if ((guard_pos.x - po.x) * next_vel.y -
-                (guard_pos.y - po.y) * next_vel.x) == 0:
-                potential_obstacles.append(guard_pos + guard_vel)
-                print("Pot Obs: " + str(potential_obstacles[-1]))
-                break
+        # # Check if a previous obstacle is to our right
+        # # Run backwards 3x from right side obstacle and 
+        # # check if we land in the same spot?
+        # for po in previous_obstacles:
+        #     if ((guard_pos.x - po.x) * next_vel.y -
+        #         (guard_pos.y - po.y) * next_vel.x) == 0:
+        #         potential_obstacles.append(guard_pos + guard_vel)
+        #         print("Pot Obs: " + str(potential_obstacles[-1]))
+        #         break
 
 # Part 1: 5404
 print("Distinct locations: "  + str(len(visited)))
-print("Potential obstacles: " + str(len(potential_obstacles)))
+#print("Potential obstacles: " + str(len(potential_obstacles)))
 
 # For part 2 what I think I should do is
 # iterate over the grid as usual, emulating
