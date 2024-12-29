f45e3c87704de48457aaa77e5a2c0886f0a1bef9
John Ginnane <John.Ginnane1@Gmail.com>
2024-12-29 20:50:09 +0000

    Attempt at improving performance, using 2 threads

--- adventofcode/2024/11/aoc_day_11.py
+++ adventofcode/2024/11/aoc_day_11.py
@@ -2,6 +2,7 @@ print("Day 11:\n")
 
 import math
 from datetime import datetime, timedelta
+from multiprocessing.pool import ThreadPool
 
 def right(s, l):
     return str(s)[-l:]
@@ -25,22 +26,29 @@ def formatTimeDelta(td:timedelta):
 
     result = (str(hours) + ":" + 
               right("00" + str(minutes), 2) + ":" +
-              right("00" + str(td.seconds) , 2) + ":")
+              right("00" + str(td.seconds) , 2) + ".")
     
-    if td.microseconds > 0 and milliseconds == 0:
+    if td.microseconds >= 0 and milliseconds == 0:
         result = "<" + result + "001"
     else:
         result = " " + result + right("000" + str(milliseconds), 3)
 
     return result
 
-def applyRules(L:list, debug:bool=False):
-    result = L.copy()
+def applyRules(L:list, debug:bool=False, start_index:int=0, end_index=None):
+    if start_index >= len(L): return []
+    if start_index < 0: start_index = 0
+    if end_index is None: end_index = len(L)-1
+    if end_index < start_index: end_index = len(L)-1
+    if end_index > len(L)-1: end_index = len(L)-1
+
+    result = L.copy()    
+    if len(result) <= 0: return []
 
     i = 0
-    length =int(math.log10(len(result)))+1
+    length=int(math.log10(len(result)))+1
 
-    while i < len(result):
+    while i <= end_index:
         ts = datetime.now()
 
         if debug: print(pad(i+1, length) + "/" + str(len(result)) + ":")
@@ -71,6 +79,7 @@ def applyRules(L:list, debug:bool=False):
                 result[i] = left
                 result.insert(i+1, right)
             i += 2
+            end_index += 1
             if debug: print("\tTook " + formatTimeDelta(datetime.now()-ts))
             continue
 
@@ -84,19 +93,40 @@ def applyRules(L:list, debug:bool=False):
     return result
 
 stones = []
-with open("test_input_11.txt", "r") as f:
+with open("input_11.txt", "r") as f:
     stones = list(map(int, f.read().split(" ")))
 
 print("Start: " + " ".join(list(map(str, stones))))
-#start_time = datetime.timestamp(datetime.now())
+start_time = datetime.now()
 ts = datetime.now()
 
+# test: 55312
 
 # Part 1: 189167 stones
-for i in range(0, 30):
-    stones = applyRules(stones)
+for i in range(0, 28):
+    from_index = 0
+    to_index = int(len(stones)/2)-1
+    ##print("From: " + str(from_index) + " to: " + str(to_index))
+
+    #stones = applyRules(stones)
+    
+    # left_half = applyRules(stones[from_index:to_index])
+    # right_half = applyRules(stones[to_index:])
+    # stones = left_half + right_half
+
+    pool = ThreadPool(processes=2)
+    left_async = pool.apply_async(applyRules, (stones[from_index:to_index],))
+    right_async = pool.apply_async(applyRules, (stones[to_index:],))
+    left_half = left_async.get()
+    right_half = right_async.get()
+    stones = left_half + right_half
+    
     newts = datetime.now()
     
-    print(pad(i+1, 3) + ") (" + formatTimeDelta(newts-ts) + ") " + str(len(stones)) + " stones")
+    #print(pad(i+1, 3) + ") (" + formatTimeDelta(newts-ts) + ") " + str(len(stones)) + " stones")
 
     ts = newts
+
+# Part 1: 189167
+print("Total stones: " + str(len(stones)))
+print("Total time: " + formatTimeDelta(datetime.now()-start_time))
