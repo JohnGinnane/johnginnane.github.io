845324a93a5dbbc1e3e45419552be4a5ba9e63e0
John Ginnane <John.Ginnane1@Gmail.com>
2025-07-06 14:50:34 +0100

    Need to turn into into "excel alpha" count

--- scripts/dijkstra-algorithm.js
+++ scripts/dijkstra-algorithm.js
@@ -2,6 +2,22 @@
 // Handy JS function to check if value is a number
 function isNumber(n) { return !isNaN(parseFloat(n)) && !isNaN(n - 0) }
 
+// Imagine base 27
+var nextNodeLetter = 1;
+
+function getNodeLetter() {
+    let result = nextNodeLetter;
+
+    nextNodeLetter = String.fromCodePoint(nextNodeLetter);
+
+    // Explode 
+
+    return result;
+}
+window.addEventListener("load", (event) => {
+    const daWorkArea = document.getElementById("da-workarea");
+});
+
 function createNode(x, y) {
     // Create div
     // Assign class
@@ -10,5 +26,12 @@ function createNode(x, y) {
     if (!isNumber(x)) { x = 0; }
     if (!isNumber(y)) { y = 0; }
 
-    
+    let radius = 10;
+
+    let newNode = document.createElement("div");
+    newNode.classList.add("da-node");
+    newNode.setAttribute("width", radius + "px");
+    newNode.setAttribute("height", radius + "px");
+
+    daWorkArea.append(newNode);
 }
