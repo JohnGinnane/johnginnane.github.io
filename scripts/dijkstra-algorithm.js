7aea206e9282d722e78b78444a0fd5fdf5b26a68
John Ginnane <John.Ginnane1@Gmail.com>
2025-07-06 15:26:45 +0100

    I think I implemented int to column name correctly

--- scripts/dijkstra-algorithm.js
+++ scripts/dijkstra-algorithm.js
@@ -14,8 +14,44 @@ function getNodeLetter() {
 
     return result;
 }
+
+function numberToExcel(n) {
+    if (!isNumber(n)) { return ""; }
+    if (n == 0) { return ""; }
+    
+    // Turn integers into Excel counting
+    // e.g.  1 ->  A
+    //       2 ->  B
+    //      ...
+    //      25 ->  Y
+    //      26 ->  Z
+    //      27 -> AA
+    //      28 -> AB etc.
+
+    // Start at 26^1 and then increment
+    // the power and divide by this value
+    // until we consumes the entire input
+    let base = 26;
+    let divisor = base;
+    let result = "";
+
+    do {
+        remainder = n % base || base;
+        result = String.fromCodePoint(64 + remainder) + result;
+        
+        n = Math.floor(n / divisor);
+        divisor *= base;
+    } while (n > 0 && remainder != base);
+
+    return result;
+}
+
 window.addEventListener("load", (event) => {
     const daWorkArea = document.getElementById("da-workarea");
+
+    for (let i = 1; i <= 300; i++) {
+        console.log(i + " -> " + numberToExcel(i));
+    }
 });
 
 function createNode(x, y) {
