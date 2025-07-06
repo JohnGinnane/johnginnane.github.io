88b6842462d100ba39858dcd1d291be60bbb0770
John Ginnane <John.Ginnane1@Gmail.com>
2025-07-06 14:34:26 +0100

    Started work on djikstra algorithm tool

---
+++ scripts/dijkstra-algorithm.js
@@ -0,0 +1,14 @@
+// https://stackoverflow.com/a/1421988
+// Handy JS function to check if value is a number
+function isNumber(n) { return !isNaN(parseFloat(n)) && !isNaN(n - 0) }
+
+function createNode(x, y) {
+    // Create div
+    // Assign class
+    // Assign event handlers
+
+    if (!isNumber(x)) { x = 0; }
+    if (!isNumber(y)) { y = 0; }
+
+    
+}
