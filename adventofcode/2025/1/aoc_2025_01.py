4cfb8df8e2e6c22a5f939dbb24528e7bc5d6b099
John Ginnane <John.Ginnane1@Gmail.com>
2025-12-19 00:09:16 +0000

    work on aoc 2025 d1p2

--- adventofcode/2025/1/aoc_2025_01.py
+++ adventofcode/2025/1/aoc_2025_01.py
@@ -1,6 +1,8 @@
 lines = open("input_01.txt", "r").readlines()
 current_number = 50
-zeroHits = 0
+zeroOnEnd = 0
+zeroTotal = 0
+last_number = current_number
 
 print("The dial starts by pointing at 50.")
 debugPrint = ""
@@ -8,6 +10,9 @@ debugPrint = ""
 for line in lines:
     direction = line[:1]
     distance = int(line[1:])
+
+    zeroTotal += int(distance/100.0)
+    distance = distance % 100
 
     if (direction == "R"):
         current_number = current_number + distance
@@ -17,26 +22,40 @@ for line in lines:
     while current_number < 0:
         current_number += 100
 
-    current_number = current_number % 100
+        if last_number != 0:
+            zeroTotal += 1
+            last_number = 0
 
-    debugPrint += f"The dial is rotated {(line.strip()+"    ")[:5]} to point at {current_number}."
+    while current_number >= 100:
+        current_number -= 100
+
+        if last_number != 0:
+            zeroTotal += 1
+            last_number = 0
 
     if current_number == 0:
-        zeroHits = zeroHits + 1
-        debugPrint += f" Hit zero {zeroHits} times!"
+        zeroOnEnd += 1
+    
+    # Need to track last number so if we 
+    # move off 0 we dont count it twice     
+    last_number = current_number
+
+    debugPrint += f"The dial is rotated {(line.strip()+"    ")[:5]} to point at {(str(current_number)+" ")[:2]}."
+
+    debugPrint += f" {zeroOnEnd}/{zeroTotal} -> {zeroOnEnd + zeroTotal}"
 
     print(debugPrint)
     debugPrint = ""
 
-
     # The dial starts by pointing at 50.
-    # The dial is rotated L68 to point at 82.
+    # The dial is rotated L68 to point at 82; during this rotation, it points at 0 once.
     # The dial is rotated L30 to point at 52.
     # The dial is rotated R48 to point at 0.
     # The dial is rotated L5 to point at 95.
-    # The dial is rotated R60 to point at 55.
+    # The dial is rotated R60 to point at 55; during this rotation, it points at 0 once.
     # The dial is rotated L55 to point at 0.
     # The dial is rotated L1 to point at 99.
     # The dial is rotated L99 to point at 0.
     # The dial is rotated R14 to point at 14.
-    # The dial is rotated L82 to point at 32.
+    # The dial is rotated L82 to point at 32; during this rotation, it points at 0 once.
+
